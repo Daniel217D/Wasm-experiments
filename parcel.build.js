39727b7533e4f78d769625de302407a1201d5740
Daniil <daniel217032001@gmail.com>
2020-11-07 12:24:04 +0300

    Update parcel.build.js

--- parcel.build.js
+++ parcel.build.js
@@ -1,7 +1,7 @@
 const Bundler = require('parcel-bundler');
 const Path = require('path');
 
-const entryFiles = Path.join(__dirname, './index.html');
+const entryFiles = Path.join(__dirname, './src/index.html');
 
 (async function() {
     await (new Bundler(entryFiles, {
